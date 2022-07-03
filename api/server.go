c9d0d3aed223d046a09e7296902f2d278b05fade
aidenmarshall <aidenmarshall36@gmail.com>
2022-07-03 21:54:03 +0800

    feat(api): create server and add createAccount api

---
+++ api/server.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	db "github.com/aidenmarshall/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// TODO: add routes to router
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
